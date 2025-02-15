250c2946f4bea37fa1431c309e1d2f0985aeb2f1
Rajpravinkumar <rajpravinkumaras@gmail.com>
2025-02-15 22:16:27 +0530

    use hook

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,9 +1,42 @@
-import React from "react";
+import {useState} from 'react'
 
-class App extends React.Component {
-  render() {
-    return <h1> Hello World!</h1>
+const App = () => {
+
+  const [count, setCount] = useState(0);
+
+  console.log(count);
+
+  const handleIncrease = () => {
+    if (count < 10) {
+      setCount(count + 1);
+    }
   }
+  
+  const handleDecrease = () => {
+   
+    if (count > 0) {
+       setCount(count -1)
+    }
+  }
+
+  const handleReset = () => {
+    setCount(0);
+  }
+return (
+    <div>
+      <h1> Counter:{count} </h1>
+      <button
+      onClick = {handleIncrease}>Increase
+
+    </button>
+    
+    <button 
+      onClick = {handleDecrease}
+    >Decrease</button>
+
+    <button onClick = {handleReset}> reset </button>
+    </div>
+  )
 }
 
-export default App;
+export default App
