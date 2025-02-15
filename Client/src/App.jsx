93a1c7b227c94d46efcb17c41c215139f4621cad
Rajpravinkumar <rajpravinkumaras@gmail.com>
2025-02-15 14:32:36 +0530

    exercise1

---
+++ Client/src/App.jsx
@@ -0,0 +1,10 @@
+import DisplayTemperature from "./component /DisplayTemperature";
+
+
+const App = () => {
+  const temperature = 20;
+
+  return <DisplayTemperature temperature={temperature } />
+}
+
+export default App
---
+++ Client/src/component /DisplayTemperature.jsx
@@ -0,0 +1,9 @@
+
+
+const DisplayTemperature = ({temperature}) => {
+  return (
+      <h1>Current Temperature : { temperature }</h1>
+  )
+}
+
+export default DisplayTemperature
