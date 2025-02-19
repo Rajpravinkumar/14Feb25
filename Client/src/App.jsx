7e3b7552d3ba3d2d3039d37c482d919d2f3706f9
Rajpravinkumar <rajpravinkumaras@gmail.com>
2025-02-19 23:20:29 +0530

    updating the dependency packages

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,42 +1,24 @@
-import {useState} from 'react'
-
 const App = () => {
 
   const [count, setCount] = useState(0);
-
-  console.log(count);
+  const [likes, setLikes] = useState(0);
+  const [dislikes, setDislikes] = useState(0);
 
   const handleIncrease = () => {
     if (count < 10) {
       setCount(count + 1);
     }
+  function handleLike() {
+    setLikes(likes + 1);
   }
-  
+
   const handleDecrease = () => {
-   
     if (count > 0) {
-       setCount(count -1)
+      setCount(count - 1);
     }
   }
-
   const handleReset = () => {
     setCount(0);
+  function handleDislike() {
+    setDislikes(dislikes + 1);
   }
-return (
-    <div>
-      <h1> Counter:{count} </h1>
-      <button
-      onClick = {handleIncrease}>Increase
-
-    </button>
-    
-    <button 
-      onClick = {handleDecrease}
-    >Decrease</button>
-
-    <button onClick = {handleReset}> reset </button>
-    </div>
-  )
-}
-
-export default App
